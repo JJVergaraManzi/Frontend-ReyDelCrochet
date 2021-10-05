7795459fed94bd208b99fb3d509a373d84d65f77
elipareja <elizabeth.pareja@mail.udp.cl>
2021-10-04 21:29:16 -0300

    xddd 4-10
    xddd 4-10

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 
 
+
+
 Vue.config.productionTip = false;
 
 
@@ -12,5 +14,6 @@ new Vue({
   router,
   store,
   vuetify,
+  el: '#app',
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Orders from '../views/Orders.vue'
-import Import from '../views/Import.vue'
+import Contacto from '../views/Contacto.vue'
+import Tienda from '../views/Tienda.vue'
 import Home from '../views/Home.vue'
+import Carritojaja from '../views/Carritojaja.vue'
 import VueAxios from 'vue-axios';
 import axios from 'axios';
+
+import Agujas from '../views/Tiendaproductos/Agujas.vue'
+import Accesorios from '../views/Tiendaproductos/Accesorios.vue'
+import Crochet from '../views/Tiendaproductos/Crochet.vue'
+import Lanas from '../views/Tiendaproductos/Lanas.vue'
+import LanasBaby from '../views/Tiendaproductos/LanasBaby.vue'
+import Palillos from '../views/Tiendaproductos/Palillos.vue'
+import Tijeras from '../views/Tiendaproductos/Tijeras.vue'
+import Trapillo from '../views/Tiendaproductos/Trapillo.vue'
 
 
 import VueFusionCharts from 'vue-fusioncharts';
@@ -19,19 +29,64 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/import',
-    name: 'import',
-    component: Import
+    path: '/tienda',
+    name: 'tienda',
+    component: Tienda
   },
   {
-    path: '/orders',
-    name: 'orders',
-    component: Orders
+    path: '/contacto',
+    name: 'contacto',
+    component: Contacto
   },
   {
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/carritojaja',
+    name: 'carritojaja',
+    component: Carritojaja
+  },
+  {
+    path: '/agujas',
+    name: 'agujas',
+    component: Agujas
+  },
+  {
+    path: '/accesorios',
+    name: 'accesorios',
+    component: Accesorios
+  },
+  {
+    path: '/crochet',
+    name: 'crochet',
+    component: Crochet
+  },
+  {
+    path: '/lanas',
+    name: 'lanas',
+    component: Lanas
+  },
+  {
+    path: '/lanasbaby',
+    name: 'lanasbaby',
+    component: LanasBaby
+  },
+  {
+    path: '/palillos',
+    name: 'palillos',
+    component: Palillos
+  },
+  {
+    path: '/tijeras',
+    name: 'tijeras',
+    component: Tijeras
+  },
+  {
+    path: '/trapillo',
+    name: 'trapillo',
+    component: Trapillo
   }
 ]
 
