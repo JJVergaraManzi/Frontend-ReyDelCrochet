94efd60cbe51c7e4f154e3448f1d3d8d94fa5e56
Fran006 <francisca.carrascob@mail.udp.cl>
2021-10-13 17:54:08 -0300

    home y navbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,42 @@
 <template>
+<section>
+  <div>
       <v-parallax 
-        height="900" 
+        height="1000"
+        width="100%" 
         dark
-        src="../assets/lanafondo3.jpg">
+        src="../assets/123.jpg">
         <v-row
           align="center"
           justify="center">
           <v-col
             class="text-center"
             cols="12">
-            <h1 class="text-h4 font-weight-thin mb-4 ">
+            <h1 class="text-h4 font-weight-normal text-dark mb-4 font-size-lg">
               El Rey Del Crochet
             </h1>
-            <h4 class="subheading">
+            <h4 class="subheading text-dark">
               Encuentra los mejores productos!
             </h4>
           </v-col>
         </v-row>
       </v-parallax>
+  </div>
+       <v-row
+          align="center"
+          >
+          <v-col
+            class="text-center"
+            cols="12">
+            <h1 class="text-h4 font-weight-thin mb-4  ">
+              Productos Destacados
+            </h1>
+            
+          </v-col>
+        </v-row>
+</section>
+
+    
 </template>
 <script>
 </script>
